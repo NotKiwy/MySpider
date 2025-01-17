846e1c1f6a406e3c43a7d9acf052e388dca733b0
NotKiwy <c3zYyyyx.offical@gmail.com>
2025-01-17 22:10:29 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function generateUsername() {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const length = Math.floor(Math.random() * 6) + 5; // От 5 до 10 символов
-    let result = '';
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return result;
-}
